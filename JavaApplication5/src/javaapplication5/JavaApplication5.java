042c3dc501f79ad8cf7cbe1b0ce82847c5d7cd8a
Erick Emmanuel Tamez Leal <erick.tamezll@gmail.com>
2016-05-04 19:30:16 -0500

    helou

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -17,7 +17,7 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         // TODO code application logic here
         //hola k hace:v
-        Syste.out.println("elerick desde github");
+        System.out.println("elerick desde github");
     }
     
 }
